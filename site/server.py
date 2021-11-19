47debff4d7b77a0f18ac31f5f34249f7db91a76d
Pete Stenger <pete@stenger.io>
2021-11-19 12:49:28 -0600

    Get communication between players working

--- site/server.py
+++ site/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, session, url_for, request
+from flask import Flask, render_template, redirect, session, url_for, current_app
 from flask_socketio import SocketIO, emit, send
 from flask_socketio import join_room, leave_room
 
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret'
 socketio = SocketIO(app)
 
+PLAYER_COUNTER = {}
 @socketio.on('move-receiver')
 def on_move(data):
     # Make sure the move works
@@ -20,18 +21,35 @@ def on_join(data):
     print(data)
     username = data['username']
     room = data['room']
+
     join_room(room)
-    emit('info', username + ' has entered the room.', to=room)
+    if room in PLAYER_COUNTER:
+        PLAYER_COUNTER[room] += 1
+    else:
+        PLAYER_COUNTER[room] = 1
 
-    emit('data', {'type': 'init' })
+    emit('status', username + ' has entered the room.', to=room)
 
+    emit('data', {'type': 'init', 'count': PLAYER_COUNTER[room] })
+@socketio.on('status')
+def on_status(data):
+    print('on_status:', data)
+    room = data['room']
+    emit('status', data['message'], to=room)
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
     room = data['room']
+    if PLAYER_COUNTER[room]:
+        PLAYER_COUNTER[room] -= 1
     leave_room(room)
     emit('status', username + ' has left the room.', to=room)
-    
+
+@socketio.on('game')
+def on_game(data):
+    room = data['room']
+    data = data['data']
+    emit('game', data, to=room)
 @socketio.on('connect')
 def test_connect():
     emit('status', ' you have connected')
